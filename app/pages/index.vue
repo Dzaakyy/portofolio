e3758de4f0367fef747acb5fcc3c96df8817567d
Dzaakyy <dzakynurwahid46@gmail.com>
2025-10-30 17:07:00 +0700

    Menambahkan background aurora dan perbaikan pillnav

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+      <div class="relative w-full h-screen flex items-center justify-center">
+    <h1 class="text-white text-3xl">
+      Halaman Utama Saya
+    </h1>
+  </div>
+</template> 
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
